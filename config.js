4fdb13efa9235307c51eeae13dcd7add866ae371
sbrocks <shaury29.baranwal@gmail.com>
2018-03-10 21:00:49 +0530

    first

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAACzfKZAR3d8BADxOQ66v8B86X0yiVLAa3lwhDcIy0LXz1lBRyA5rfgwHTvmo9rtoyOtyKw7ZCV45dt5wlJ48QwjUEAZA7nSceINKqRmUqfn4YtHqPrDqmQnlvsNxsZAg2zo8Ar7QsVC6JAnXr45pQEG0Nyj0257V1ZA5B5lJhSltmuMWDRvd',
+    FB_VERIFY_TOKEN: 'iamyourbiggestfan',
+    API_AI_CLIENT_ACCESS_TOKEN: 'b69b3a0641db48e88c88686e86d40387',
+    FB_APP_SECRET: '8bd2cc3cc419c1fe0bbbddd46270f02f',
+    SERVER_URL: "https://sbchatbot.herokuapp.com/",
+};
